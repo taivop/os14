052722c9a1d48d81a4da9d8c713a2f571a705cc2
taivop <tpungas@ut.ee>
2014-10-26 22:16:40 +0200

    changing processes data structure, adding function to sort by field

--- src/parse.py
+++ src/parse.py
@@ -3,19 +3,38 @@ s = "0,10;4,5;12,4"
 # INPUT
 # pattern string, consisting of >1 semicolon-separated pairs
 # each pair consists of process arrive time and process length
+# OUTPUT
+# tuples (id, arrive_time, duration)
 def testPatternToArrays(s):
     pairs = s.strip().split(";")
 
-    arrived = []
-    duration = []
+    processes = []
 
+    id = 0
     for pair_str in pairs:
         pair = pair_str.strip().split(",")
-        arrived.append(int(pair[0]))
-        duration.append(int(pair[1]))
+        processes.append((id, int(pair[0]), int(pair[1])))
+        id += 1
 
-    # input checks
-    assert len(arrived) == len(duration)
-    assert len(arrived) != 0
+    # result checks
+    assert len(processes) != 0
 
-    return arrived, duration
+    # sort by arrive time
+
+    return processes
+
+# make copy of processes array and return sorted by required field
+def sortProcessesByField(processes, field):
+    if isinstance(field, str):          # add support for referencing by name
+        if field.lower() == "id":
+            field = 0
+        elif field.lower() in ["arrive_time", "arrived", "arrive", "arr"]:
+            field = 1
+        elif field.lower() in ["duration", "dur"]:
+            field = 2
+        else:       # unrecognised field key => sort by ID
+            field = 0
+    return sorted(processes, key=lambda tup: tup[field])
+
+
+print(sortProcessesByField(testPatternToArrays(s), "duration"))
