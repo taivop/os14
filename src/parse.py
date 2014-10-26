031fafcef2522ecb6c79ddf5593bb7c8e885be16
taivop <tpungas@ut.ee>
2014-10-26 22:20:09 +0200

    added started field to processes list

--- src/parse.py
+++ src/parse.py
@@ -4,7 +4,7 @@ s = "0,10;4,5;12,4"
 # pattern string, consisting of >1 semicolon-separated pairs
 # each pair consists of process arrive time and process length
 # OUTPUT
-# tuples (id, arrive_time, duration)
+# list of tuples (id, arrive_time, duration, started_time)
 def testPatternToArrays(s):
     pairs = s.strip().split(";")
 
@@ -13,7 +13,7 @@ def testPatternToArrays(s):
     id = 0
     for pair_str in pairs:
         pair = pair_str.strip().split(",")
-        processes.append((id, int(pair[0]), int(pair[1])))
+        processes.append((id, int(pair[0]), int(pair[1]), None))
         id += 1
 
     # result checks
@@ -32,8 +32,10 @@ def sortProcessesByField(processes, field):
             field = 1
         elif field.lower() in ["duration", "dur"]:
             field = 2
-        else:       # unrecognised field key => sort by ID
-            field = 0
+        elif field.lower() in ["started", "sta"]:
+            field = 3
+        else:       # unrecognised field key => throw error
+            raise Exception("Unknown field name")
     return sorted(processes, key=lambda tup: tup[field])
 
 
